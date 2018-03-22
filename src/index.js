6dc90a86b024c8bdd9329f6ff6d68a4091829d85
Aaron Happe <ah239752@stl1403009mac-2.local>
2018-03-22 12:27:17 -0500

    starting egghead course

---
+++ src/index.js
@@ -0,0 +1,8 @@
+console.log('asdf')
+
+	const rootElement = document.getElementById('#root');
+	console.log(rootElement);
+	// const element = document.createElement('div');
+	// element.textContent = 'Hello Word';
+	// element.className = 'container';
+	// rootElement.appendChild(element);
